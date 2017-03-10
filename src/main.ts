0220fdfde2d45bbc677c5a85b0e3453c93f8b811
Oscar Lemström <oscar.lemstrom@gmail.com>
2017-03-10 15:01:15 +0200

    Add rabbitmq listeners that support Rx streams

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import * as Koa from 'koa';
 import * as json from 'koa-json';
 
 import { routes } from './routes';
+import { publish, listen } from './queue';
 
 const app = new Koa();
+
+console.log('Starting catalog service');
+
+global.setInterval(() => {
+    publish('date', Date.now().toString());
+}, 1200);
+
+listen('date').subscribe(v => console.log('endval', v));
+
 
 app.use(json({ pretty: false }));
 app.use(routes);
