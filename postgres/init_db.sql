110366e79e9cbd2c1a0cc035edafe743d169cea3
Oscar Lemström <oscar.lemstrom@gmail.com>
2017-03-10 15:01:16 +0200

    Start modeling item database

--- postgres/init_db.sql
+++ postgres/init_db.sql
@@ -1,4 +1,16 @@
 CREATE TABLE items (
-  id          char(5)  PRIMARY KEY,
-  name        TEXT NOT NULL
+  id          varchar(10) PRIMARY KEY,
+  name        TEXT NOT NULL,
+  description TEXT NOT NULL,
+  current_price INT DEFAULT 0,
+  state TEXT
 );
+
+CREATE TABLE categories (
+  name TEXT PRIMARY KEY
+);
+
+CREATE TABLE categories_in_items (
+  category_id TEXT REFERENCES categories (name) ON UPDATE CASCADE ON DELETE CASCADE,
+  item_id TEXT REFERENCES items (id) ON UPDATE CASCADE ON DELETE CASCADE
+)
