32f03b73eac86332a6388838f6e79063e1cf27b4
Oscar Lemström <oscar.lemstrom@gmail.com>
2017-03-10 15:01:15 +0200

    Add JSON support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import * as Koa from 'koa';
+import * as json from 'koa-json';
 
 import { routes } from './routes';
 
 const app = new Koa();
 
+app.use(json({ pretty: false }));
 app.use(routes);
 app.listen(process.env.PORT || '8080');
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,22 @@
 import * as KoaRouter from 'koa-router';
 import { Context } from 'koa'
 
+interface Item {
+    id: string;
+    name: string;
+    description?: string;
+}
+
+const items: Item[] = [
+    { id: '2', name: 'Potato'},
+    { id: '1', name: 'Potato2'},
+    { id: '3', name: 'Potato3'}
+];
+
+
 const router = new KoaRouter();
 router.get('/', function* (next: Context) {
-    this.body = 'Hello world';
+    this.body = items;
     yield next;
 });
 
